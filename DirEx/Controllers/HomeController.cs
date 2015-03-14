0b6d653fe2c874f175d4a3ddb1cfe15f4ea8fc37
Nathanial Woolls <nathanial.woolls@idmworks.com>
2015-03-14 02:37:03 -0400

    Refactor controller code

--- DirEx/Controllers/HomeController.cs
+++ DirEx/Controllers/HomeController.cs
@@ -1,28 +1,35 @@
 ﻿using System;
 using System.DirectoryServices;
 using System.Web.Mvc;
+using DirEx.Models;
 
 namespace DirEx.Controllers
 {
 	public class HomeController : Controller
 	{
-		public ActionResult Index()
-		{			
-			var viewModel = new Models.DirectoryViewModel();
+		private const string host = "idfdemo.dev.idmworks.net";
+		private const int port = 6389;
+		private const string username = "cn=idfRacfAdmin,dc=racf,dc=com";
+		private const string password = "idfRacfPwd";
 
-			var root = "dc=racf,dc=com";
-			var host = "idfdemo.dev.idmworks.net";
-			var port = 6389;
-			var username = "cn=idfRacfAdmin,dc=racf,dc=com";
-			var password = "idfRacfPwd";
+		public ActionResult Index()
+		{
+			var baseDn = "dc=racf,dc=com";
+
+			var viewModel = PopulateDirectoryEntries(baseDn);
+
+			return View(viewModel);
+		}
+
+		private DirectoryViewModel PopulateDirectoryEntries(string root)
+		{
+			var viewModel = new DirectoryViewModel();
 
 			var server = "LDAP://" + host + ":" + port + "/";
 
 			var dir = new DirectoryEntry(server + root);
 			if (String.IsNullOrEmpty(username))
-			{
 				dir.AuthenticationType = AuthenticationTypes.Anonymous;
-			}
 			else
 			{
 				dir.Username = username;
@@ -41,18 +48,18 @@ namespace DirEx.Controllers
 			foreach (SearchResult result in results)
 			{
 				var child = result.GetDirectoryEntry();
-				var entry = new Models.EntryViewModel();
+				var entry = new EntryViewModel();
 
 				// not accessing child.Name directly here as the object may be a malformed LDAP entry
 				// this is currently the case with the RACF connector and ou=Aliases
 				// this will at least let us populate the entry and we can error fetching details later
 				entry.RelativeName = child.Path.Substring(server.Length, child.Path.Length - server.Length - viewModel.DistinguishedName.Length - 1);
 
-                entry.DistinguishedName = entry.RelativeName + "," + viewModel.DistinguishedName;
+				entry.DistinguishedName = entry.RelativeName + "," + viewModel.DistinguishedName;
 				viewModel.Entries.Add(entry);
 			}
-			
-			return View(viewModel);
+
+			return viewModel;
 		}
 	}
 }
