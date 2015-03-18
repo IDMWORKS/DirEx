4053cd850b5029f9da3015964467cc1a846e20b4
Nathanial Woolls <nathanial.woolls@idmworks.com>
2015-03-18 15:37:59 -0400

    Introduce support for Bootstrap alerts

---
+++ DirEx/Keys/TempData.cs
@@ -0,0 +1,10 @@
+﻿namespace DirEx.Keys
+{
+	public class TempData
+	{
+		public const string AlertSuccess = "Bootstrap-Alert-Success";
+		public const string AlertInfo = "Bootstrap-Alert-Info";
+		public const string AlertWarning = "Bootstrap-Alert-Warning";
+		public const string AlertDanger = "Bootstrap-Alert-Danger";
+	}
+}
