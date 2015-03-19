d53d35cab1eab15769e611fc1bade586121911c3
Nathanial Woolls <nathanial.woolls@idmworks.com>
2015-03-19 02:22:27 -0400

    Scroll to the current tree entry using JS rather than a URL fragment
    Using a URL fragument causes both the sidebar and content divs to scroll down

---
+++ DirEx.Web/Extensions/HtmlExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace DirEx.Web.Extensions
+{
+	public static class HtmlExtensions
+	{
+		public static string ToHtmlId(this string source)
+		{
+			return source.GetHashCode().ToString();
+		}
+	}
+}
