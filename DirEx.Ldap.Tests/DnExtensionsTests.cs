cdb0278333298292c82b310c64adaf65771446de
Nathanial Woolls <nathanial.woolls@idmworks.com>
2015-03-24 20:49:26 -0400

    Introduce a test for DnExtensions.IsDn()

---
+++ DirEx.Ldap.Tests/DnExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using DirEx.Ldap.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DirEx.Ldap.Tests
+{
+	[TestClass]
+	public class DnExtensionsTests
+	{
+		[TestMethod]
+		// should not raise an exception
+		public void IsDn_ShaHash_ReturnsFalse()
+		{
+			// arrange
+			const string value = "{SHA}W6ph5Mm5Pz8GgiULbPgzG37mj9g=";
+			const bool expected = false;
+
+			// act
+			bool actual = value.IsDn();
+
+			// assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ DirEx.Ldap.Texts/DnExtensionsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DirEx.Ldap.Texts
+{
+	[TestClass]
+	public class DnExtensionsTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
