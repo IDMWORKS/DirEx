30e7c7010eaa3b2e06596bed22b3728e64f606ab
Nathanial Woolls <nathanial.woolls@idmworks.com>
2015-03-24 20:19:07 -0400

    Detect and link to email addresses, phone numbers, and ADsPaths

--- DirEx.Ldap/Data/ModelNames.cs
+++ DirEx.Ldap/Data/ModelNames.cs
@@ -7,5 +7,7 @@
 		public const string OrganizationUnit = "ou";
 		public const string DomainComponent = "dc";
 		public const string Organization = "o";
-	}
+		public const string TelephoneNumber = "telephonenumber";
+		public const string ADsPath = "adspath";
+    }
 }
---
+++ DirEx.Web/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DirEx.Web.Extensions
+{
+	public static class StringExtensions
+	{
+		public static bool IsEmailAddress(this string source)
+		{
+			return new EmailAddressAttribute().IsValid(source);
+		}
+	}
+}
